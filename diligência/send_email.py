cea1b72ea8f2aabe91130b0b47895ab49098f3b8
mortified-hugo <hugo.sjrp@gmail.com>
2021-01-14 12:16:13 -0300

    Added E-mail automation

--- diligência/send_email.py
+++ diligência/send_email.py
@@ -15,10 +15,11 @@ with open("emails/analistas.json", mode='r') as file:
 print(analistas)
 df = pd.read_excel(f'output/diligência_{hoje}.xlsx')
 for analista in set(df["Análise Macro Analisada por:"]):
-    diligencias = f'output/for_emails/diligência_{analistas[analista]["nome"]}_{hoje}.xlsx'
+    diligencias = f'output/for_emails/diligencia_{analistas[analista]["nome"]}_{hoje}.xlsx'
     df[df["Análise Macro Analisada por:"] == analista].to_excel(
-        diligencias)
-    send_email('hugo.figueira@cidadania.gov.br',
+        diligencias,
+    index=False)
+    send_email(analistas[analista]["email"],
                f"Tramitação de Processos LECOM - Resposta de Diligência - {hoje}",
                f"Bom dia {analistas[analista]['nome']},\n\n"
                f"Segue em anexo tramitação de processos com resposta de diligência:\n\n"
