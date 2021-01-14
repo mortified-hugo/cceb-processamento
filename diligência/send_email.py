0e293b4640e94aa8523af4a5fdcaa711af13937c
mortified-hugo <hugo.sjrp@gmail.com>
2021-01-14 14:04:58 -0300

    Desenvolvimento da automação de envio de email

--- diligência/send_email.py
+++ diligência/send_email.py
@@ -12,22 +12,31 @@ with open("emails/analistas.json", mode='r') as file:
     analistas = json.load(file)
     analistas["Karine Gonçalves de Souza"] = analistas['Karine GonÃ§alves de Souza']
 
-print(analistas)
-df = pd.read_excel(f'output/diligência_{hoje}.xlsx')
-for analista in set(df["Análise Macro Analisada por:"]):
-    diligencias = f'output/for_emails/diligencia_{analistas[analista]["nome"]}_{hoje}.xlsx'
-    df[df["Análise Macro Analisada por:"] == analista].to_excel(
-        diligencias,
-    index=False)
-    send_email(analistas[analista]["email"],
-               f"Tramitação de Processos LECOM - Resposta de Diligência - {hoje}",
-               f"Bom dia {analistas[analista]['nome']},\n\n"
-               f"Segue em anexo tramitação de processos com resposta de diligência:\n\n"
-               f"Att,\n"
-               f"Hugo",
-               str(pathlib.Path(diligencias).resolve()))
+try:
+    df = pd.read_excel(f'output/diligência_{hoje}.xlsx')
 
-print("Emails enviados")
+    for analista in set(df["Análise Macro Analisada por:"]):
+        diligencias = f'output/for_emails/diligencia_{analistas[analista]["nome"]}_{hoje}.xlsx'
+        df[df["Análise Macro Analisada por:"] == analista].to_excel(
+            diligencias,
+            index=False)
+        send_email(analistas[analista]["email"],
+                   f"Tramitação de Processos LECOM - Resposta de Diligência - {hoje}",
+                   f"Bom dia {analistas[analista]['nome']},\n\n"
+                   f"Segue em anexo tramitação de processos com resposta de diligência:\n\n"
+                   f"Att,\n"
+                   f"Hugo",
+                   str(pathlib.Path(diligencias).resolve()))
+
+    print("Emails enviados")
+
+except FileNotFoundError:
+    print("DILIGÊNCIAS NÃO ENCONTRADAS")
+
+
+
+
+
 
 
 
